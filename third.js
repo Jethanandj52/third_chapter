2304297e00d9612758896c03c1c90057d2a16132
Jethanandj52 <134337163+Jethanandj52@users.noreply.github.com>
2024-10-30 23:05:29 +0500

    Add files via upload

---
+++ third.js
@@ -0,0 +1,20 @@
+
+// first
+var age=15;
+ var ageString='I am '
+ var ageString2=" Years old"
+
+ alert("Question 1 \n"+ageString+age+ageString2)
+
+// second
+var time=2;
+alert("Question 2 \n You have visited this site "+time+" time")
+// third
+var birhtYear=2002
+document.write('"<b>Question 3 </b><br><br>"My birth year is ' +birhtYear+'<br>Data type of my declared varaible is '+typeof(birhtYear)+"<br>")
+
+// fourth
+var vistorName="John Doe";
+var productTitle='T-shirt';
+var quantity=5;
+document.write( "<b>Question 4 </b><br><br> "+vistorName+' ordered '+quantity+' '+productTitle+'(s) on XYZ Clothing store');
